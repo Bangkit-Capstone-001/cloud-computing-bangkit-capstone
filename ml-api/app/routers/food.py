98b74012c81f946ed50e1bf9f68e2c37785c2cb3
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2024-06-14 23:30:20 +0700

    feat: add preprocess rgba to rgb

--- ml-api/app/routers/food.py
+++ ml-api/app/routers/food.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, File, UploadFile, HTTPException
+from PIL import Image
+import io
 from ..services.foodDetection import predict_food
 
 router = APIRouter()
@@ -17,8 +19,19 @@ ACCEPTED_IMAGE_TYPES = [
 async def predict(image: UploadFile = File(...)):
     if image.content_type not in ACCEPTED_IMAGE_TYPES:
         raise HTTPException(status_code=404, detail="Invalid file type. Only images are allowed.")
-    
+
     image_bytes = await image.read()
-    [label, confidence] = predict_food(image_bytes)
+    image = Image.open(io.BytesIO(image_bytes))
+
+    # Check if the image has an alpha channel (RGBA)
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+
+    # Convert image back to bytes
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format=image.format)
+    img_byte_arr = img_byte_arr.getvalue()
+
+    [label, confidence] = predict_food(img_byte_arr)
 
     return {"food": label, "confidence": confidence*100}
